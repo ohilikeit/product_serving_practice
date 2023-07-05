1104d809880c82303a34dff67644f7b68ec13d79
ohilikeit <yjh091500@naver.com>
2023-07-05 15:02:40 +0900

    feat: add parameters and bodies

---
+++ fastapi_examples/04_optional_parameter.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from typing import Optional
+import uvicorn
+
+app = FastAPI()
+
+fake_items_db = [{'item_name': 'Foo'},
+                 {'item_name': 'Bar'},
+                 {'item_name': 'Baz'},]
+
+@app.get("/items/{item_id}")
+def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id}
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
